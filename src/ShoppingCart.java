99c5ef851e7725d8f16c4a9c77e94d97f2345c21
IsuruGimhana <145018121+IsuruGimhana@users.noreply.github.com>
2023-11-25 17:43:10 +0530

    updated comments

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -1,2 +1,29 @@
+/**
+ * This class represents the user shopping cart
+ *
+ * @author Isuru Gimhana
+ */
+import java.util.ArrayList;
+
 public class ShoppingCart {
+    private ArrayList<Product> cartList;
+
+    public ShoppingCart() {
+        this.cartList = new ArrayList<>();
+    }
+    public void addProduct(Product product) {
+        this.cartList.add(product);
+    }
+
+    public void removeProduct(Product product) {
+        this.cartList.remove(product);
+    }
+
+    public double getTotalCost() {
+        double totalCost = 0;
+        for (Product product : cartList) {
+            totalCost += product.getProductPrice();
+        }
+        return totalCost;
+    }
 }
---
+++ src/ShoppingManager.java
@@ -0,0 +1,6 @@
+public interface ShoppingManager {
+    public abstract void addProduct(Product product);
+    public abstract void deleteProduct(Product product);
+    public abstract void getProductList();
+    public abstract void saveProductList();
+}
